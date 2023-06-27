f560651cc1c947ec3cb3b8f90e74169ccf0ee7bb
Taevon Jordan <39418407+taevonjordan11@users.noreply.github.com>
2023-06-27 17:26:38 -0400

    refactored event event handler

--- alien_invasion.py
+++ alien_invasion.py
@@ -33,17 +33,25 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                    if event.key == pygame.K_RIGHT:
-                        self.ship.moving_right = False
-                    elif event.key == pygame.K_LEFT:
-                        self.ship.moving_left = False
-                    # Move Ship to the right.
-                    self.ship.rect.x += 1
+                self._check_keyup_events(event)
+                # Move Ship to the right.
+                self.ship.rect.x += 1
+
+    def _check_keydown_events(self, event):
+        """Respond to keydown events."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     # Redraw screen during each pass through the loop.
     def _update_screen(self):     
